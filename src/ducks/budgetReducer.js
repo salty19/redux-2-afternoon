f5d3fc74a14896028a8c95e31abe0ef5abfdc765
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-09 13:19:42 -0700

    setting up store.js

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+}
+
+
+export default function reducer(state = initialState, action) {
+
+    return state
+}
