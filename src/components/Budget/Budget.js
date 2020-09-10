9b22b27e38dfc6e3266756029423eec98caa59ae
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-09 20:33:54 -0700

    Step 6 complete

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -9,16 +9,18 @@ import Nav from './../shared/Nav';
 import './Budget.css';
 import { connect } from 'react-redux' 
 import { requestUserData } from './../../ducks/userReducer'
+import { requestBudgetData } from './../../ducks/budgetReducer'
 
 
 class Budget extends Component {
   componentDidMount() {
     this.props.requestUserData()
+    this.props.requestBudgetData()
   }
 
   render() {
 
-    const { loading } = this.props.budget 
+    const { loading, purchases, budgetLimit } = this.props.budget 
     const { firstName, lastName } = this.props.user
 
     return (
@@ -29,11 +31,11 @@ class Budget extends Component {
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
-              <DisplayPurchases />
+              <DisplayPurchases purchases={purchases} />
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -49,4 +51,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, {requestUserData} )(Budget)
+export default connect(mapStateToProps, {requestUserData, requestBudgetData} )(Budget)
--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -1,3 +1,7 @@
+import axios from 'axios'
+import { Switch } from 'react-router-dom'
+
+
 const initialState = {
     purchases: [],
     budgetLimit: null,
@@ -5,7 +9,26 @@ const initialState = {
 }
 
 
-export default function reducer(state = initialState, action) {
+const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA' 
 
-    return state
+export function requestBudgetData() {
+    let data = axios.get('/api/budget-data').then(res => res.data)
+
+    return {
+        type: REQUEST_BUDGET_DATA, 
+        payload: data
+    }
+}
+
+
+export default function reducer(state = initialState, action) {
+    switch(action.type) {
+        case REQUEST_BUDGET_DATA + '_PENDING':
+            return {...state, loading: true}
+        case REQUEST_BUDGET_DATA + '_FULFILLED':
+            return {...state, ...action.payload, loading: false}
+        default:
+            return state 
+
+    }
 }
